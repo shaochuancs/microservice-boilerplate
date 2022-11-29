d16e4e2b43fc55273784ec6ad7ba49ab84e090e5
Chuan Shao <shaochuancs@gmail.com>
2022-11-29 09:03:40 +0800

    add spring boot service and node.js service

---
+++ service-spring-boot/src/test/java/com/shaochuancs/servicespringboot/ServiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaochuancs.servicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
